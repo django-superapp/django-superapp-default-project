3787c7701ee1070f41b8003502136f426442f54f
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2024-10-30 11:34:49 +0200

    Update settings.py

--- superapp/settings.py
+++ superapp/settings.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 import os
+from logging.handlers import SysLogHandler
+
 from os import environ
 from pathlib import Path
 
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 from django.utils.translation import gettext_lazy as _
+
 from django_superapp.settings import extend_superapp_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,22 +28,21 @@ ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = environ.get("SECRET_KEY", get_random_secret_key())
 
-DEBUG = environ.get("DEBUG", "false") == "true"
+DEBUG = environ.get("DEBUG", 'false') == 'true'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "").split(",") + ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "").split(",") + ['localhost', '127.0.0.1']
 ALLOWED_HOSTS = [host for host in ALLOWED_HOSTS if host.strip()]
 if DEBUG:
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = environ.get("CSRF_TRUSTED_ORIGINS", "").split(",") + [
-    "http://localhost:8080",
-    "http://localhost:3000",
-]
+CSRF_TRUSTED_ORIGINS = environ.get(
+    "CSRF_TRUSTED_ORIGINS", ""
+).split(",") + ['http://localhost:8080', 'http://localhost:3000']
 CSRF_TRUSTED_ORIGINS = [host for host in CSRF_TRUSTED_ORIGINS if host.strip()]
 
 ######################################################################
@@ -56,7 +57,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
-    "debug_toolbar",
 ]
 
 ######################################################################
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -79,81 +80,91 @@ MIDDLEWARE = [
 ######################################################################
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
-ROOT_URLCONF = "superapp.urls"
+ROOT_URLCONF = 'superapp.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "superapp" / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'superapp' / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "superapp.wsgi.application"
+WSGI_APPLICATION = 'superapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 ######################################################################
 # Caches
 ######################################################################
-CACHES = (
-    {
-        "default": {
-            "BACKEND": "django.core.cache.backends.redis.RedisCache",
-            "LOCATION": os.environ.get("REDIS_BROKER_URL"),
-        }
+CACHES = {
+    'default': {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.environ.get('REDIS_BROKER_URL'),
     }
-    if os.environ.get("REDIS_BROKER_URL", "") != ""
-    else {
-        "default": {
-            "BACKEND": "django.core.cache.backends.db.DatabaseCache",
-            "LOCATION": "cache_table",
-        }
+} if os.environ.get('REDIS_BROKER_URL', '') != '' else {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'cache_table',
     }
-)
+}
 
 # Always log to console
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
-    "formatters": {
-        "verbose": {"format": "[contactor] %(levelname)s %(asctime)s %(message)s"},
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
     },
-    "handlers": {
+    'formatters': {
+        'verbose': {
+            'format': '[contactor] %(levelname)s %(asctime)s %(message)s'
+        },
+    },
+    'handlers': {
         # Send all messages to console
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        # Send info messages to syslog
+        'syslog':{
+            'level':'INFO',
+            'class': 'logging.handlers.SysLogHandler',
+            'facility': SysLogHandler.LOG_LOCAL2,
+            'address': '/dev/log',
+            'formatter': 'verbose',
         },
         # Warning messages are sent to admin emails
-        "mail_admins": {
-            "level": "WARNING",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler",
+        'mail_admins': {
+            'level': 'WARNING',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
         },
     },
-    "loggers": {
+    'loggers': {
         # This is the "catch all" logger
-        "": {
-            "handlers": [
-                "console",
-                "mail_admins",
-            ],
-            "level": "DEBUG",
-            "propagate": False,
+        '': {
+            'handlers': ['console', 'syslog', 'mail_admins',],
+            'level': 'DEBUG',
+            'propagate': False,
         },
-    },
+    }
 }
 
 ######################################################################
@@ -161,14 +172,16 @@ LOGGING = {
 ######################################################################
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Bucharest"
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
 USE_TZ = True
 
-LANGUAGES = (("en", _("English")),)
-LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
+LANGUAGES = (
+    ("en", _("English")),
+)
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 ######################################################################
 # Static
@@ -205,5 +218,7 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 # SUPERAPP dynamic settings
 ######################################################################
 from . import apps as superapp_apps
-
-extend_superapp_settings(main_settings=globals(), superapp_apps=superapp_apps)
+extend_superapp_settings(
+    main_settings=globals(),
+    superapp_apps=superapp_apps
+)
